0cf2f7f999d5b1e3c39b908880f12330674f9d15
Fachryhura12 <96608181+Fachryhura12@users.noreply.github.com>
2021-12-25 17:09:14 +0700

    Add files via upload

---
+++ bot.sh
@@ -0,0 +1,102 @@
+clear
+sleep 1
+echo "===================================="|lolcat
+echo "||AUTHOR = FACHRY                 ||"|lolcat
+echo "||Team = FACHRY TEAM              ||"|lolcat
+echo "===================================="|lolcat
+echo
+echo
+echo "tunggu [\]"|lolcat
+sleep 5
+figlet "selesai"|lolcat
+echo
+echo
+echo "mulai untuk menuyul"|lolcat
+echo
+read -p "masukan id >" bry    |lolcat
+if [ $bry = 1356789276 ]
+then
+echo "tunggu [\]"|lolcat
+sleep 4
+echo "operations berhasil :)"|lolcat
+echo
+echo
+fi
+	read -p "jumlah >" by
+	if [ $by = 1 ]
+	then
+	sleep 4
+	echo "berhasil mendapatkan 50"|lolcat
+	sleep 7
+	echo "habis bro"|lolcat
+	fi
+
+	if [ $by = 2 ]
+	then
+	sleep 4
+	echo "berhasil mendapatkan 50"|lolcat
+	sleep 7
+	echo "berhasil mendapatkan 50"|lolcat
+	sleep 7
+	echo "habis bro"|lolcat
+	fi
+
+	if [ $by = 3 ]
+	then
+	sleep 4
+	echo "berhasil mendapatkan 50"|lolcat
+	sleep 7
+	echo "berhasil mendapatkan 50"|lolcat
+	sleep 7
+	echo "berhasil mendapatkan 50"|lolcat
+	sleep 7
+	echo "habis bro"|lolcat
+	fi
+
+	if [ $by = 4 ]
+	then
+	sleep 4
+	echo "berhasil mendapatkan 50"|lolcat
+	sleep 7
+	echo "berhasil mendapatkan 50"|lolcat
+	sleep 7
+	echo "berhasil mendapatkan 50"|lolcat
+	sleep 7
+	echo "berhasil mendapatkan 50"|lolcat
+	sleep 7
+	echo "habis bro"|lolcat
+	fi
+
+	if [ $by = 5 ]
+	then
+	sleep 4
+	echo "berhasil mendapatkan 50"|lolcat
+	sleep 7
+	echo "berhasil mendapatkan 50"|lolcat
+	sleep 7
+	echo "berhasil mendapatkan 50"|lolcat
+	sleep 7
+	echo "berhasil mendapatkan 50"|lolcat
+	sleep 7
+	echo "berhasil mendapatkan 50"|lolcat
+	sleep 7
+	echo "habis bro"|lolcat
+	fi
+
+	if [ $by = 6 ]
+	then
+	sleep 4
+	echo "berhasil mendapatkan 50"|lolcat
+	sleep 7
+	echo "berhasil mendapatkan 50"|lolcat
+	sleep 7
+	echo "berhasil mendapatkan 50"|lolcat
+	sleep 7
+	echo "berhasil mendapatkan 50"|lolcat
+	sleep 7
+	echo "berhasil mendapatkan 50"|lolcat
+	sleep 7
+	echo "berhasil mendapatkan 50"|lolcat
+	sleep 7
+	echo "habis bro"|lolcat
+	fi
